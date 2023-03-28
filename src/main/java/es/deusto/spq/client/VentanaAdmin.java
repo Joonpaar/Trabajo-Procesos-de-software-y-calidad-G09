92e7a3f2eb9adf2f817af368cc4cf735745a7f76
Mikel <mikel.lambarri@opendeusto.es>
2023-03-28 21:14:12 +0200

    Tabla para el catalogo de productos

--- src/main/java/es/deusto/spq/client/VentanaAdmin.java
+++ src/main/java/es/deusto/spq/client/VentanaAdmin.java
@@ -3,10 +3,17 @@ package es.deusto.spq.client;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import es.deusto.spq.server.jdo.Producto;
+
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -14,6 +21,10 @@ import javax.swing.JButton;
 public class VentanaAdmin extends JFrame {
 
 	private JPanel contentPane;
+	private JTable tablaProductos;
+	public static DefaultTableModel modeloTablaProductos;
+	public static List<Producto> productos;
+
 
 	/**
 	 * Launch the application.
@@ -57,6 +68,14 @@ public class VentanaAdmin extends JFrame {
 		
 		JButton btnAnyadir = new JButton("AÑADIR");
 		panel_2.add(btnAnyadir);
+		
+		modeloTablaProductos=new DefaultTableModel();
+		String [] nombreColumnas = {"Nombre", "Tipo", "Stock", "Precio"};
+		modeloTablaProductos.setColumnIdentifiers(nombreColumnas);
+		//TODO bucle con todos los productos para la tabla
+		tablaProductos = new JTable(modeloTablaProductos);
+		JScrollPane scrollTablaProductos = new JScrollPane(tablaProductos);
+		panel_1.add(scrollTablaProductos);
 		
 		btnAnyadir.addActionListener(new ActionListener() {
 			
