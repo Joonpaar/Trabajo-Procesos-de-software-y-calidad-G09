addcece67de3a98688793dc74b84de455fbd0805
edularrii <eduardo.larrinaga@opendeusto.es>
2023-03-30 11:30:01 +0200

    Nueva Clase Utilidades añadida y cambios en VentanaLogin
    Clase Utilidades añadida, con funciones para los componentes swing. Cambios en la ventana aplicando también las nuevas funciones de la clase Utilidades

---
+++ src/main/java/es/deusto/spq/client/Utilidades.java
@@ -0,0 +1,163 @@
+package es.deusto.spq.client;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.Popup;
+import javax.swing.PopupFactory;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+
+public class Utilidades {
+	public static Popup popup;
+	
+	public static void modifyButtonOpcion(JButton boton) {
+		boton.setForeground(Color.WHITE);
+		boton.setBackground(new Color(255, 127, 80));
+		boton.setBorderPainted(false);
+		boton.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		boton.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				boton.setBackground(new Color(255, 127, 80));
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				boton.setBorderPainted(true);
+
+			}
+
+		});
+	}
+	
+	public static void modifyButton(JButton boton) {
+		boton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		boton.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		boton.setForeground(new Color(67, 67, 67));
+		boton.setFocusPainted(false);
+		boton.setBackground(Color.white);	
+
+		boton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent evt) {
+				boton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				boton.setContentAreaFilled(true);
+			}
+		});
+	}
+	
+	public static void JLabelWithPopup(JLabel label) {
+		label.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseEntered(MouseEvent evt) {
+				if (popup != null) {
+					popup.hide();
+				}
+				JLabel text = new JLabel("Campo requerido");
+				text.setFont(new Font("Segoe UI", Font.BOLD, 11));
+				text.setForeground(Color.WHITE);
+				text.setOpaque(true);
+				text.setBackground(new Color(20, 115, 191));
+				popup = PopupFactory.getSharedInstance().getPopup(evt.getComponent(), text,
+						(int) label.getLocationOnScreen().getX()-100, (int) label.getLocationOnScreen().getY());
+				popup.show();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if (popup != null) {
+					popup.hide();
+				}
+			}
+		});
+	}
+	
+	public static JPasswordField modifyPasswordField(JPasswordField field) {
+		Border line = BorderFactory.createLineBorder(new Color(194, 194, 194), 1);
+		Border empty = new EmptyBorder(0, 5, 0, 0);
+		CompoundBorder border = new CompoundBorder(line, empty);
+		field.setBorder(border);
+		field.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		field.setForeground(Color.GRAY);
+		field.setEchoChar((char) 0);
+
+		field.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				Border line = BorderFactory.createLineBorder(new Color(20, 115, 191), 1);
+				Border empty = new EmptyBorder(0, 5, 0, 0);
+				CompoundBorder border = new CompoundBorder(line, empty);
+				field.setBorder(border);
+				field.setForeground(Color.BLACK);
+				super.focusGained(e);
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				Border line = BorderFactory.createLineBorder(new Color(194, 194, 194), 1);
+				Border empty = new EmptyBorder(0, 5, 0, 0);
+				CompoundBorder border = new CompoundBorder(line, empty);
+				field.setBorder(border);
+				field.setForeground(Color.GRAY);
+				super.focusLost(e);
+			}
+		});
+		return field;
+	}
+	
+	public static JTextField modifyTextField(JTextField field) {
+		Border line = BorderFactory.createLineBorder(new Color(194, 194, 194), 1);
+		Border empty = new EmptyBorder(0, 5, 0, 0);
+		CompoundBorder border = new CompoundBorder(line, empty);
+		field.setBorder(border);
+		field.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		field.setForeground(Color.GRAY);
+
+		field.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				Border line = BorderFactory.createLineBorder(new Color(20, 115, 191), 1);
+				Border empty = new EmptyBorder(0, 5, 0, 0);
+				CompoundBorder border = new CompoundBorder(line, empty);
+				field.setBorder(border);
+				field.setForeground(Color.BLACK);
+				super.focusGained(e);
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				Border line = BorderFactory.createLineBorder(new Color(194, 194, 194), 1);
+				Border empty = new EmptyBorder(0, 5, 0, 0);
+				CompoundBorder border = new CompoundBorder(line, empty);
+				field.setBorder(border);
+				field.setForeground(Color.GRAY);
+				super.focusLost(e);
+			}
+		});
+
+		field.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				field.setCursor(new Cursor(Cursor.TEXT_CURSOR));
+			}
+
+		});
+		return field;
+	}
+}
--- src/main/java/es/deusto/spq/client/VentanaLogin.java
+++ src/main/java/es/deusto/spq/client/VentanaLogin.java
@@ -1,12 +1,22 @@
 package es.deusto.spq.client;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.font.TextAttribute;
+import java.util.Map;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.ws.rs.client.Entity;
@@ -24,8 +34,6 @@ import javax.swing.SwingConstants;
 public class VentanaLogin {
 
 	JFrame frame;
-	private JTextField textFieldUsuario;
-	private JTextField textFieldContraseña;
 
 	/**
 	 * Launch the application.
@@ -57,48 +65,81 @@ public class VentanaLogin {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 		
+		//Panel principal
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
+		//Boton
 		JButton btnLogin = new JButton("Iniciar Sesión");
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnLogin.setBounds(102, 147, 117, 21);
+		btnLogin.setBounds(165, 170, 117, 21);
+		Utilidades.modifyButton(btnLogin);
 		panel.add(btnLogin);
 		
-		textFieldUsuario = new JTextField();
-		textFieldUsuario.setBounds(144, 56, 96, 19);
+		//Componentes de texto y demas
+		JTextField textFieldUsuario = new JTextField();
+		textFieldUsuario.setBounds(200, 56, 115, 19);
 		panel.add(textFieldUsuario);
+		Utilidades.modifyTextField(textFieldUsuario);
 		textFieldUsuario.setColumns(10);
 		
-		textFieldContraseña = new JTextField();
-		textFieldContraseña.setBounds(144, 101, 96, 19);
-		panel.add(textFieldContraseña);
-		textFieldContraseña.setColumns(10);
+		JPasswordField passwordFieldContraseña = new JPasswordField();
+		passwordFieldContraseña.setBounds(200, 101, 115, 19);
+		panel.add(passwordFieldContraseña);
+		Utilidades.modifyPasswordField(passwordFieldContraseña);
+		passwordFieldContraseña.setColumns(10);
 		
 		JLabel lblUsuario = new JLabel("Usuario:");
 		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
-		lblUsuario.setBounds(34, 60, 67, 13);
+		lblUsuario.setBounds(100, 60, 67, 13);
+		lblUsuario.setFont(new Font("Segoe UI", Font.BOLD, 13));
 		panel.add(lblUsuario);
 		
 		JLabel lblContraseña = new JLabel("Contraseña:");
 		lblContraseña.setHorizontalAlignment(SwingConstants.CENTER);
-		lblContraseña.setBounds(34, 105, 81, 13);
+		lblContraseña.setBounds(100, 105, 81, 13);
+		lblContraseña.setFont(new Font("Segoe UI", Font.BOLD, 13));
 		panel.add(lblContraseña);
 		
-		JButton btnRegistro = new JButton("REGISTRARSE");
-		btnRegistro.setBounds(102, 189, 117, 23);
-		panel.add(btnRegistro);
+		JLabel loginlabel = new JLabel("¿No tienes cuenta? Regístrate");
+		loginlabel.setFont(new Font("Segoe UI", Font.BOLD, 13));
+		loginlabel.setBounds(135, 210, 180, 23);
+		panel.add(loginlabel);
 		
+		//Acciones de los componentes
 		btnLogin.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				ExampleClient.loginUser(textFieldUsuario.getText(), textFieldContraseña.getText());
+				ExampleClient.loginUser(textFieldUsuario.getText(), passwordFieldContraseña.getText());
+			}
+		});
+		// MouseListener loginlabel
+		loginlabel.addMouseListener(new MouseAdapter() {
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			public void mouseEntered(MouseEvent evt) {
+				Font font = loginlabel.getFont();
+				Map attributes = font.getAttributes();
+				attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+				loginlabel.setFont(font.deriveFont(attributes));
+				loginlabel.setForeground(new Color(20,115,191));
+				loginlabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+
+			public void mouseExited(MouseEvent evt) {
+				loginlabel.setFont(new Font("Segoe UI", Font.BOLD, 13));
+				loginlabel.setForeground(Color.BLACK);
+			}
+		});
+				
+		loginlabel.addMouseListener(new MouseAdapter() {	
+			@Override
+			public void mouseClicked(MouseEvent e) {
+//				ventanaActual.dispose();
+//				VentanaLogin v1 = new VentanaLogin(MainProgram.loginController);
+//				v1.setVisible(true);	
 			}
 		});
 	}
